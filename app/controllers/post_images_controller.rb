2df261346856c3bfcd36f99650cbfe8f48f95b6a
vagrant <vagrant@localhost.localdomain>
2020-07-13 11:34:06 +0000

    [Finish] 5-4

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,17 +2,24 @@ class PostImagesController < ApplicationController
 	def new
 		@post_image = PostImage.new
   	end
+
   	def create
   		@post_image = PostImage.new(post_image_params)
-  		@post_image.user_id = current_user.user_id
-  		@post_image.save
-  		redirect_to post_images_path
+  		@post_image.user_id = current_user.id
+  		if @post_image.save
+  		  redirect_to post_images_path
+      else
+        render :new
+      end
   	end
+
   	def index
-  		@post_images = PostImage.all
+  		@post_images = PostImage.page(params[:page]).reverse_order
   	end
+    
   	def show
   		@post_image = PostImage.find(params[:id])
+      @post_comment = PostComment.new
   	end
 
   	private
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b87821047f3ccf3a7343996026c409a2950554fe14dd1f3f84d383da3698faf4c8ebb78f178d3fe4316d6c499d6e2a0a78698d7ada3136eb4bcdb71911543d6c'
